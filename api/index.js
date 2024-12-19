52df771189fb96c39148e8e0b5bbe3e192a69394
Selezen Andrew <aselezen624@gmail.com>
2024-12-19 16:31:04 +0200

    0.3

--- api/index.js
+++ api/index.js
@@ -17,10 +17,23 @@ mongoose
 app.use(express.json());
 
 app.use("/admin", adminRouter);
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-app.get("/", (req, res) =>{
-  res.send("welcome from flowers api")
-})
+app.get("/", (req, res) => {
+  res.send("welcome from flowers api");
+});
 
 app.listen(PORT, (error) => {
   if (error) {
